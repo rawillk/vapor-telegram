1a532489547c64bc2001e5c670e3a0aa8e986233
Ravil Khusainov <ravil@partof.team>
2023-05-12 14:50:15 +0300

    Inline keyboard support

---
+++ Sources/Telegram/Keyboard/InlineKeyboard.swift
@@ -0,0 +1,24 @@
+
+import Vapor
+
+public struct InlineKeyboardMarkup: Content {
+    
+    let inline_keyboard: [[InlineKeyboardButton]]
+    
+    public init(rows: [[InlineKeyboardButton]]) {
+        self.inline_keyboard = rows
+    }
+}
+
+public struct InlineKeyboardButton: Codable {
+    
+    public let text: String
+    public let callback_data: String?
+    public let url: String?
+    
+    public init(text: String, callback_data: String?, url: String?) {
+        self.text = text
+        self.callback_data = callback_data
+        self.url = url
+    }
+}
--- Sources/Telegram/Models/Message.swift
+++ Sources/Telegram/Models/Message.swift
@@ -15,11 +15,13 @@ public extension Message {
     struct Out: Content {
         
         public let chat_id: String
-        public let text: String
+        public let text: String?
+        public let reply_markup: InlineKeyboardMarkup?
         
-        public init(chat_id: String, text: String) {
+        public init(chat_id: String, text: String?, keyboard: InlineKeyboardMarkup? = nil) {
             self.chat_id = chat_id
             self.text = text
+            self.reply_markup = keyboard
         }
     }
     
@@ -28,5 +30,13 @@ public extension Message {
         let prefix = "/"
         guard text.hasPrefix(prefix) else { return nil }
         return .init(rawValue: text)
+    }
+    
+    func reply(with text: String, keyboard: InlineKeyboardMarkup? = nil) -> Out {
+        .init(chat_id: "\(chat.id)", text: text)
+    }
+    
+    func reply(with keyboard: InlineKeyboardMarkup) -> Out {
+        .init(chat_id: "\(chat.id)", text: nil, keyboard: keyboard)
     }
 }

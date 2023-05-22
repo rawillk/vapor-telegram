887d1c18a84da3c7e15f31bf0374670e8b66b20b
Ravil Khusainov <zver.kaban@gmail.com>
2023-05-23 01:02:19 +0300

    Callback Query Id type fix

--- Sources/Telegram/Models/CallbackQuery.swift
+++ Sources/Telegram/Models/CallbackQuery.swift
@@ -3,7 +3,7 @@ import Vapor
 
 public struct CallbackQuery: Content {
     
-    public let id: Int64
+    public let id: String
     public let from: User
     public let message: Message?
     public let data: String?
@@ -13,7 +13,7 @@ public extension CallbackQuery {
 
     struct Answer: Content {
 
-        let callback_query_id: Int64
+        let callback_query_id: String
         let text: String?
         let show_alert: Bool?
     }
